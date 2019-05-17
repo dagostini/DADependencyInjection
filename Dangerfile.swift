3e2e20a70881a33a1c3370712e31816b3b86079f
Dejan Agostini <dejan.agostini@gmail.com>
2019-05-17 11:18:21 +0100

    Minor update.

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -3,13 +3,14 @@ let danger = Danger()
 
 // Pull out the edited files
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
+let hasChangelogEntry = danger.git.modifiedFiles.contains("CHANGELOG.md")
 
 // Example from danger
 // Let people bail from the CHANGELOG check
 let hasSkipChangelog = danger.github.pullRequest.body.contains("#no_changelog")
 
 // Request for a CHANGELOG entry with each app change
-if editedFiles.count > 0 && !hasSkipChangelog {
+if editedFiles.count > 0 && !hasChangelogEntry && !hasSkipChangelog {
     fail("Please add a CHANGELOG entry for these changes. If you would like to skip this check, add `#no_changelog` to the PR body and re-run CI.")
 }
 
